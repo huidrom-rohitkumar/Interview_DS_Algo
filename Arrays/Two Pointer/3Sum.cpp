8849b9566dacccd288a5ccc10a50eeea3dce45d2
Huidrom Rohitkumar Meitei <rohithuidrom2020@gmail.com>
2026-07-30 09:25:37 +0530

    Update 3Sum.cpp

--- Arrays/Two Pointer/3Sum.cpp
+++ Arrays/Two Pointer/3Sum.cpp
@@ -1,6 +1,6 @@
-/*        Scroll down to see JAVA code also            */
+
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=_cBWWebTVpI&list=PLpIkg8OmuX-K6A0sEPFxOSJh4_AjCGAFf&index=4
+    YOUTUBE VIDEO  : https://www.youtube.com/watch?v=_cBWWebTVpI&list=PLpIkg8OmuX-K6A0sEPFxOSJh4_AjCGAFf&index=4
     Company Tags                : Amazon, Facebook, Google
     Question on GfG             : Find triplets with zero sum (https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1)
     Leetcode Qn Link            : https://leetcode.com/problems/3sum/
@@ -46,49 +46,3 @@ public:
 };
 
 
-/************************************************ JAVA ************************************************/
-//T.C : O(n^2)
-//S.C : O(1)
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length < 3) {
-            return new ArrayList<>();
-        }
-
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            twoSum(nums, i + 1, result, -nums[i]);
-        }
-
-        return result;
-    }
-
-    private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
-        int i = k, j = nums.length - 1;
-
-        while (i < j) {
-            if (nums[i] + nums[j] > target) {
-                j--;
-            } else if (nums[i] + nums[j] < target) {
-                i++;
-            } else {
-                result.add(Arrays.asList(-target, nums[i], nums[j]));
-
-                while (i < j && nums[i] == nums[i + 1]) {
-                    i++;
-                }
-                while (i < j && nums[j] == nums[j - 1]) {
-                    j--;
-                }
-
-                i++;
-                j--;
-            }
-        }
-    }
-}
