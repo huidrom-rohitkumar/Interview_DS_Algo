713928d5584938bc4d89e3f5e09de60f8955e446
Huidrom Rohitkumar Meitei <rohithuidrom2020@gmail.com>
2026-08-08 14:50:13 +0530

    Update Count Number of Bad Pairs.cpp

--- HashMap/Count Number of Bad Pairs.cpp
+++ HashMap/Count Number of Bad Pairs.cpp
@@ -1,8 +1,24 @@
-/*     Scroll below to see JAVA code also    */
+
 /*
     MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=quUIQ2pw5EU
-    Company Tags                : Will update soon
     Leetcode Link               : https://leetcode.com/problems/count-number-of-bad-pairs
+*/
+
+/*
+
+Video Summary :
+Transforming the Array:
+
+Modify nums[i] to nums[i] - i so that the problem reduces to finding equal values instead of explicitly checking pair conditions.
+This transformation helps identify "good pairs" efficiently.
+Using a HashMap for Frequency Counting:
+
+Maintain a frequency map to track occurrences of transformed values.
+If a value has appeared before, it contributes to "good pairs," reducing the need for brute force comparison.
+Calculating Bad Pairs Efficiently:
+
+Instead of iterating over all pairs, compute the total possible pairs (j, i) implicitly.
+Use the count of previously seen values to determine how many pairs are "good," and subtract from the total to get "bad pairs."
 */
 
 
@@ -28,6 +44,7 @@ public:
         return result;
     }
 };
+
 
 //Approach-2 (Using hashmap)
 //T.C : O(n)
@@ -89,75 +106,3 @@ public:
 };
 
 
-/************************************************************ Java *****************************************************/
-//Approach-1 (Brute Force)
-//T.C : O(n^2)
-//S.C : O(1)
-class Solution {
-    public long countBadPairs(int[] nums) {
-        int n = nums.length;
-        long result = 0;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (j - i != nums[j] - nums[i]) {
-                    result++;
-                }
-            }
-        }
-        
-        return result;
-    }
-}
-
-
-//Approach-2 (Using hashmap)
-//T.C : O(n)
-//S.C : O(n)
-class Solution {
-    public long countBadPairs(int[] nums) {
-        int n = nums.length;
-        long result = 0;
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = nums[i] - i;
-        }
-
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(nums[0], 1);
-
-        for (int j = 1; j < n; j++) {
-            int countOfNumsj = map.getOrDefault(nums[j], 0);
-            int totalNumsBeforej = j;
-            int badPairs = totalNumsBeforej - countOfNumsj;
-            result += badPairs;
-            
-            map.put(nums[j], countOfNumsj + 1);
-        }
-
-        return result;
-    }
-}
-
-//Approach-3 (Using hashmap)
-//T.C : O(n)
-//S.C : O(n)
-class Solution {
-    public long countBadPairs(int[] nums) {
-        int n = nums.length;
-        long result = 0;
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            int diff = nums[i] - i;
-            int totalPairsTillIndex = i;
-            int goodPairs = mp.getOrDefault(diff, 0);
-            
-            result += (totalPairsTillIndex - goodPairs);
-            
-            mp.put(diff, goodPairs + 1);
-        }
-        
-        return result;
-    }
-}
